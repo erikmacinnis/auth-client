33e182a155217f894e698fd5c33b6105d7624499
erikmacinnis <erikhyun35@gmail.com>
2023-07-26 09:37:40 -0300

    feat(ephemeral, regular, relay_list)

--- ephemeral.js
+++ ephemeral.js
@@ -10,15 +10,14 @@ const {
 sk = "9cb4b3c5e96101d6d621a898fae7eb0f34505086bcca67a668c0e61d0725be1b"
 pk = "c8af1bed462df07ad87e1678eeab9ebeb742e4cec52fdf771ca1a9214eca04c3"
 
+deletedEventId = "466f6c543decee597bcdcf2e83fde3eba6fa726e36771f9418ba061ad633590d"
+
 let event = {
-    kind: 10002,
+    kind: 23456,
     pubkey: pk,
     created_at: Math.floor(Date.now() / 1000),
     tags: [
-        ["r", "wss://alicerelay.example.com"],
-        ["r", "wss://brando-relay.com"],
-        // ["r", "wss://expensive-relay.example2.com", "write"],
-        ["r", "wss://nostr-relay.example.com", "read"],
+        ["e", deletedEventId],
     ],
     content: '',
 }
